bf0dd78503ed9fb626a7da5617d4d315f4fc7570
Erick201424 <98076338+Erick201424@users.noreply.github.com>
2023-12-06 12:18:05 -0600

    Fix port

--- src/database/mariadb.ts
+++ src/database/mariadb.ts
@@ -14,8 +14,44 @@ export const pool = mariadb.createPool({
     user: process.env.DB_USER,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306,
+    port: 3306,
 })
+
+export async function createTables() {
+    let tablesCreated = false;
+    try {
+        const conn = await pool.getConnection();
+
+        // Comprueba si las tablas ya existen
+        const checkUsersQuery = "SHOW TABLES LIKE 'users';";
+        const checkAddressQuery = "SHOW TABLES LIKE 'address';";
+
+        const users = await conn.query(checkUsersQuery);
+        const address = await conn.query(checkAddressQuery);
+
+        // Si no se encontraron resultados, las tablas no existen y se pueden crear
+        if (Array.isArray(users) || Array.isArray(address)) {
+            if (users.length === 0 || address.length === 0) {
+                const initializeFilePath = path.join(__dirname, 'initialize.sql');
+                const initializeFileContent = fs.readFileSync(initializeFilePath, "utf-8");
+                const sqlStatements = initializeFileContent.split(';');
+
+                for (const sqlStatement of sqlStatements) {
+                    if (sqlStatement.trim() !== '') {
+                        await conn.query(sqlStatement);
+                    }
+                }
+                tablesCreated = true;
+            } else {
+                tablesCreated = false;
+            }
+        }
+        conn.release();
+    } catch (error) {
+        signale.error("Error al crear las tablas:", error);
+    }
+    return tablesCreated;
+}
 
 export async function query(sql: string, params: any[]) {
     try {
